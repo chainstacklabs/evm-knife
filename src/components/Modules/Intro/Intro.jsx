a5dcd0c0bd87e4f140f60f049d1efe6864d10bba
Ilya Kolebayev <kolebayev@gmail.com>
2023-08-28 19:11:20 +0200

    Update copy on Intro

--- src/components/Modules/Intro/Intro.jsx
+++ src/components/Modules/Intro/Intro.jsx
@@ -34,22 +34,22 @@ const Intro = () => {
         {[
           {
             name: 'Smart contracts event tools',
-            text: 'Event logs are a type of log entry in the Ethereum blockchain that contains information about events that have been triggered by smart contracts. Event logs are created when a contract emits an event in Solidity.',
+            text: 'Easily work with event logs. Generate event signatures and encode event topics for efficient log filtering. Ideal for real-time monitoring, debugging, and analytics.',
             icon: () => <Image src={scEvents} height={60} width={60} />,
           },
           {
             name: 'Solidity CALLDATA tools',
-            text: 'In Solidity, CALLDATA refers to the input data that is sent along with a function call to a contract on the Ethereum blockchain. When you invoke a function on a smart contract, you can provide arguments or parameters to that function. These arguments, along with information about the function being called, are encoded into a hexadecimal string known as the CALLDATA, and it is included in the transaction that invokes the function.',
+            text: 'Enhance your smart contract development with our Solidity calldata utilities. Generate function signatures for contract interaction and encode calldata parameters. A must-have for seamless contract execution and data management.',
             icon: () => <Image src={solidity} height={60} width={60} />,
           },
           {
             name: 'Smart contract tools',
-            text: 'A smart contract is a self-executing digital contract with the terms of the agreement directly written into code. It runs on a blockchain platform, most commonly associated with the Ethereum blockchain, but also present in other blockchain networks like Binance Smart Chain, Cardano, and more. The term "smart contract" was first coined by computer scientist and legal scholar Nick Szabo in 1994.',
+            text: 'Simplify your blockchain development workflow with our ABI and Code Retrieval Tool. Input a contract address to instantly fetch its ABI and source code. Essential for contract interaction, auditing, and reverse engineering.',
             icon: () => <Image src={sc} height={60} width={60} />,
           },
           {
-            name: 'Converter',
-            text: 'Just converting data from one type to another.',
+            name: 'Converters',
+            text: 'Streamline your development process with our all-in-one Blockchain Value Converter. Effortlessly convert between Wei, Gwei, and Ether, or transform hex to decimal and vice versa. Also includes Keccak-256 hashing and checksum address generation. A comprehensive tool for all your blockchain conversion needs.',
             icon: () => <Image src={converter} height={60} width={60} />,
           },
         ].map((item, idx) => {
